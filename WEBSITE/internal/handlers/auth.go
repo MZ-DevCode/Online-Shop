a17a448d7b236ce1b61e452dd210232abc85cf50
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-08-22 06:37:30 +0200

    fixed auth.go

--- WEBSITE/internal/handlers/auth.go
+++ WEBSITE/internal/handlers/auth.go
@@ -4,10 +4,11 @@ import (
 	"WEBSITE/internal/database"
 	"WEBSITE/internal/models"
 	"WEBSITE/internal/utils"
+	"database/sql"
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
-	"os/user"
 )
 
 // RegisterHandler обрабатывает регистрацию нового пользователя
@@ -37,56 +38,74 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == "POST" {
-		username := r.FormValue("username")
-		password := r.FormValue("password")
+		u := models.User{
+			Username: r.FormValue("username"),
+			Password: r.FormValue("password"),
+		}
+
 		repeatPassword := r.FormValue("repeatPassword")
-		if password != repeatPassword {
+		if u.Password != repeatPassword {
 			http.Error(w, "Пароли не совпадают", http.StatusBadRequest)
 			return
 		}
 
-		hashedPassword, err := utils.HashPassword(password)
+		hashedPassword, err := utils.HashPassword(u.Password)
 		if err != nil {
 			http.Error(w, "Ошибка шифрования пароля", http.StatusInternalServerError)
 			return
 		}
-
-		user := models.User{
-			Username: username,
-			Password: hashedPassword,
-		}
+		u.Password = hashedPassword
 
 		query := "INSERT INTO users (username, password) VALUES (?, ?)"
-		_, err = database.DB.Exec(query, user.Username, user.Password)
+		_, err = database.DB.Exec(query, u.Username, u.Password)
 		if err != nil {
 			http.Error(w, "Ошибка регистрации", http.StatusBadRequest)
 			return
 		}
+		w.Write([]byte("Регистрация успешна!"))
+	}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tmpl, err := template.ParseFiles("templates/login.html")
+		if err != nil {
+			log.Println("Ошибка загрузки шаблона:", err)
+			http.Error(w, "Ошибка загрузки шаблона", http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, nil)
+		return
 	}
 
-	func LoginHandler(w http.ResponseWriter, r *http.Request){
-		if r.Method == "GET" {
-			tmpl, err := template.ParseFiles("templates/login.html")
-			if err != nil {
-				log.Println("Ошибка загрузки шаблона:", err)
-				http.Error(w, "Ошибка загрузки шаблона", http.StatusInternalServerError)
+	if r.Method == "POST" {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		var hashedPassword string
+
+		query := "SELECT password FROM users WHERE username = ?"
+		err := database.DB.QueryRow(query, username).Scan(&hashedPassword)
+
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				log.Println("Ошибка поиска пользователя:", err)
+				http.Error(w, "Неверное имя пользователя или пароль", http.StatusUnauthorized)
 				return
 			}
-			tmpl.Execute(w, nil)
+
+			log.Println("Ошибка БД:", err)
+			http.Error(w, "Ошибка поиска пользователя", http.StatusInternalServerError)
 			return
 		}
 
-		if r.Method == "POST"{
-			username := r.FormValue("username")
-			password := r.FormValue("password")
-
-			query := database.DB.Query("SELECT password FROM users WHERE username = ?")
-			passw, err := database.DB.Exec(query, user.Username)
-
-			if passw != password{
-				http.Error(w, "Неверный пароль", http.StatusBadRequest)
-			}
-
+		if !utils.CheckPasswordHash(password, hashedPassword) {
+			log.Println("Неверный пароль для пользователя:", username)
+			http.Error(w, "Неверное имя пользователя или пароль", http.StatusUnauthorized)
+			return
 		}
+
+		w.Write([]byte("Успешная авторизация!"))
+
 	}
 }
