68fa2d4e874800ed0fdbd3e722ab89c5ada49a05
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-08-25 16:04:09 +0200

    update models

--- WEBSITE/internal/models/user.go
+++ WEBSITE/internal/models/user.go
@@ -2,6 +2,8 @@ package models
 
 type User struct {
 	ID       int    `db:"id"`
+	UUID     string `db:"uuid"`
 	Username string `db:"username"`
+	Name     string `db:"name"`
 	Password string `db:"password"`
 }
