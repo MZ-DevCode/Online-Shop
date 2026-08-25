e00857680f5f2147e4afd4336b4f35df7fb89121
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-08-25 16:02:25 +0200

    update sql request

--- WEBSITE/internal/database/db.go
+++ WEBSITE/internal/database/db.go
@@ -28,8 +28,9 @@ func InitDB() {
 	createTableQuery := `
 		CREATE TABLE IF NOT EXISTS users (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			uuid VARCHAR(11) TEXT NOT NULL,
-			username TEXT NOT NULL,
+			uuid VARCHAR(11)UNIQUE NOT NULL,
+			username TEXT UNIQUE NOT NULL,
+			name TEXT NOT NULL,
 			password TEXT NOT NULL
 		);`
 
