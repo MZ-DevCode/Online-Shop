0cde63fff389aebecdbae94e641986c2dbf36602
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-08-26 09:21:22 +0200

    database

--- WEBSITE/internal/handlers/catalog.go
+++ WEBSITE/internal/handlers/catalog.go
@@ -1,9 +1,28 @@
 package handlers
 
 import (
+	"WEBSITE/internal/database"
+	"WEBSITE/internal/models"
 	"net/http"
 )
 
 func CatalogHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		rows, err := database.DB.Query("SELECT id, name, price, stock FROM products")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		defer rows.Close()
 
+		Product := models.Product{
+			ID:    id,
+			Name:  name,
+			Price: price,
+			Stock: stock,
+		}
+	}
+
+	if r.Method == "POST" {
+
+	}
 }
