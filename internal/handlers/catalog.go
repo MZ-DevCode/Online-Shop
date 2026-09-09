2ad15fd5b28e69632cd142b383224c6fe42364db
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-09 17:44:57 +0200

    added configuration for server

--- internal/handlers/catalog.go
+++ internal/handlers/catalog.go
@@ -89,9 +89,4 @@ func ShowCart(w http.ResponseWriter, r *http.Request) {
 
 	tmpl.Execute(w, products)
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
 }
