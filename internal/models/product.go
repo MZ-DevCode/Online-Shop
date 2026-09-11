6071c6bdf4c7747f5dd628bee6973cd183c6552a
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-11 17:56:02 +0200

    fix show cart

--- internal/models/product.go
+++ internal/models/product.go
@@ -8,3 +8,16 @@ type Product struct {
 	Stock       int     `json:"stock"`
 	Description string  `json:"description"`
 }
+
+type CartItem struct {
+	ID             int     `json:"id"`
+	Name           string  `json:"name"`
+	Price          float64 `json:"price"`
+	Quantity       int     `json:"quantity"`
+	TotalItemPrice float64 `json:"total_item_price"`
+}
+
+type CartPageData struct {
+	Items      []CartItem `json:"items"`
+	TotalPrice float64    `json:"total_price"`
+}
