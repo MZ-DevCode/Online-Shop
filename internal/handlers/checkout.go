77d16ed24c007733cc432c47ec1ed999eceee9cf
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-15 11:52:42 +0200

    update file

--- internal/handlers/checkout.go
+++ internal/handlers/checkout.go
@@ -1,6 +1,10 @@
 package handlers
 
 import (
+	"WEBSITE/internal/database"
+	"context"
+	"database/sql"
+	"errors"
 	"html/template"
 	"net/http"
 )
@@ -11,6 +15,28 @@ func (c *Context) CheckoutHandler() {
 		if err := cartTmpl.Execute(c.W, data); err != nil{
 			c.Error("Ошибка отображения")
 		}
+
+		userUUID, err := c.getUserUUIDFromSession()
+		if err != nil{
+			c.Redirect("/login")
+		}
+
+		ctx, cancel := context.WithTimeout(c.Ctx, 3 * rime.Second)
+		defer cancel()
+
+		tx, err := database.DB.BeginTx(ctx, nil)
+		if err != nil{
+			c.Error(err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		defer func(){
+			if err := tx.Rollback(); err != nil && !errors.Is(err, sql.ErrTxDone){
+				log.Printf("Ошибка отката транзакции: %v", err)
+			}
+		}()
+
+
 
 			c.Redirect("/cart")
 	}
