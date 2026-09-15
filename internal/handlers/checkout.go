cfb0a01ff974d5ee3b70c710f4cdaa33d76a949e
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-15 12:03:47 +0200

    -

--- internal/handlers/checkout.go
+++ internal/handlers/checkout.go
@@ -5,43 +5,47 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"html/template"
+	"log"
 	"net/http"
+	"time"
 )
 
 func (c *Context) CheckoutHandler() {
 	switch c.R.Method {
 	case "POST":
-		if err := cartTmpl.Execute(c.W, data); err != nil{
+		if err := cartTmpl.Execute(c.W, data); err != nil {
 			c.Error("Ошибка отображения")
 		}
 
 		userUUID, err := c.getUserUUIDFromSession()
-		if err != nil{
+		if err != nil {
 			c.Redirect("/login")
 		}
 
-		ctx, cancel := context.WithTimeout(c.Ctx, 3 * rime.Second)
+		ctx, cancel := context.WithTimeout(c.Ctx, 3*time.Second)
 		defer cancel()
 
 		tx, err := database.DB.BeginTx(ctx, nil)
-		if err != nil{
+		if err != nil {
 			c.Error(err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		defer func(){
-			if err := tx.Rollback(); err != nil && !errors.Is(err, sql.ErrTxDone){
+		defer func() {
+			if err := tx.Rollback(); err != nil && !errors.Is(err, sql.ErrTxDone) {
 				log.Printf("Ошибка отката транзакции: %v", err)
 			}
 		}()
 
+		rows, err := tx.QueryContext(ctx, "SELECT с.quantity, p.price FROM cart c JOIN products p ON c.product_id = p.id WHERE id = user_uuid", userUUID)
+		for rows.Next() {
 
+		}
 
-			c.Redirect("/cart")
-	}
+		c.Redirect("/cart")
 
 	default:
 		c.Error("Method not allowed", http.StatusMethodNotAllowed)
 	}
+
 }
